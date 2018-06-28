baa53aa30539212907c243889ef45c94a129667d
serged97 <pessokho@gmail.com>
2018-06-28 23:08:40 +0100

    second commit

---
+++ app/src/main/java/jourmal/app/journalapp/MainActivity.java
@@ -0,0 +1,17 @@
+package jourmal.app.journalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * journal-app-b68ed
+ * */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
